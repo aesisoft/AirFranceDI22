823a9fd997f38f61c13cf6c752c04859b386993e
Nadine CLAVERE <nadine.clavere@equadex.net>
2024-01-24 15:58:06 +0100

    ajout console de test de l'authentification

---
+++ ClientConsoleTest/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Text;
+
+const string baseAddress = "https://localhost:7139/";
+HttpClient client = new() { BaseAddress = new Uri(baseAddress) };
+
+
+// Login
+var jsonString = "{ \"email\": \"nadine.clavere@equadex.net\", \"password\": \"MotDePasse@2024\" }";
+var httpContent = new StringContent(jsonString, Encoding.UTF8, "application/json");
+var response = await client.PostAsync("login?useCookies=true&useSessionCookies=true", httpContent);
+Console.WriteLine(response);
+
+
+// Accès autorisé
+HttpClient client2 = new() { BaseAddress = new Uri(baseAddress) };
+response = await client2.GetAsync("WeatherForecast");
+if (response.IsSuccessStatusCode)
+{
+    string resultat = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(resultat);
+}
+else Console.WriteLine(response);
+
+
+Console.ReadKey();

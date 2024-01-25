cf214a9135d2d21ced6f30de24efebcfdf5508a3
Nadine CLAVERE <nadine.clavere@equadex.net>
2024-01-25 14:22:41 +0100

    ajout du projet web avec authentification

---
+++ AirFranceDI22webSecure/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using AirFranceDI22Model.Dao;
+using AirFranceDI22webSecure.Models;
+using AirFranceDI22webSecure.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirFranceDI22webSecure.Controllers;
+
+public class AccountController(SignInManager<User> signInManager, UserManager<User> userManager) : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            //login
+            var result = await signInManager.PasswordSignInAsync(model.Username!, model.Password!, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+        return View(model);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterVM model)
+    {
+        if (ModelState.IsValid)
+        {
+            User user = new()
+            {
+                //Name = model.Name,
+                UserName = model.Email,
+                Email = model.Email,
+                //Address = model.Address
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password!);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+        return View(model);
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return RedirectToAction("Index","Home");
+    }
+}
---
+++ AirFranceDI22webSecure/Controllers/VolsController.cs
@@ -0,0 +1,174 @@
+﻿using AirFranceDI22Model.Context;
+using AirFranceDI22Model.Dao;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirFranceDI22webSecure.Controllers
+{
+    [Authorize]
+    public class VolsController : Controller
+    {
+        private readonly AirFranceDI22Context _context;
+
+        public VolsController(AirFranceDI22Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Vols
+        public async Task<IActionResult> Index()
+        {
+            var airFranceDI22Context = _context.Vols.Include(v => v.AeroportArrivee).Include(v => v.AeroportDepart).Include(v => v.Compagnie);
+            return View(await airFranceDI22Context.ToListAsync());
+        }
+
+        // GET: Vols/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vol = await _context.Vols
+                .Include(v => v.AeroportArrivee)
+                .Include(v => v.AeroportDepart)
+                .Include(v => v.Compagnie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vol == null)
+            {
+                return NotFound();
+            }
+
+            return View(vol);
+        }
+
+        // GET: Vols/Create
+        public IActionResult Create()
+        {
+            ViewData["AeroportArriveeId"] = new SelectList(_context.Aeroports, "Id", "Nom");
+            ViewData["AeroportDepartId"] = new SelectList(_context.Aeroports, "Id", "Nom");
+            ViewData["CompagnieId"] = new SelectList(_context.Compagnies, "Id", "Nom");
+            return View();
+        }
+
+        // POST: Vols/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NumeroVol,OuvertResa,DateHeureDepart,DateHeureArrivee,CompagnieId,AeroportDepartId,AeroportArriveeId")] Vol vol)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vol);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AeroportArriveeId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportArriveeId);
+            ViewData["AeroportDepartId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportDepartId);
+            ViewData["CompagnieId"] = new SelectList(_context.Compagnies, "Id", "Nom", vol.CompagnieId);
+            return View(vol);
+        }
+
+        // GET: Vols/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vol = await _context.Vols.FindAsync(id);
+            if (vol == null)
+            {
+                return NotFound();
+            }
+            ViewData["AeroportArriveeId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportArriveeId);
+            ViewData["AeroportDepartId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportDepartId);
+            ViewData["CompagnieId"] = new SelectList(_context.Compagnies, "Id", "Nom", vol.CompagnieId);
+            return View(vol);
+        }
+
+        // POST: Vols/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NumeroVol,OuvertResa,DateHeureDepart,DateHeureArrivee,CompagnieId,AeroportDepartId,AeroportArriveeId")] Vol vol)
+        {
+            if (id != vol.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vol);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VolExists(vol.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AeroportArriveeId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportArriveeId);
+            ViewData["AeroportDepartId"] = new SelectList(_context.Aeroports, "Id", "Nom", vol.AeroportDepartId);
+            ViewData["CompagnieId"] = new SelectList(_context.Compagnies, "Id", "Nom", vol.CompagnieId);
+            return View(vol);
+        }
+
+        // GET: Vols/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vol = await _context.Vols
+                .Include(v => v.AeroportArrivee)
+                .Include(v => v.AeroportDepart)
+                .Include(v => v.Compagnie)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vol == null)
+            {
+                return NotFound();
+            }
+
+            return View(vol);
+        }
+
+        // POST: Vols/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vol = await _context.Vols.FindAsync(id);
+            if (vol != null)
+            {
+                _context.Vols.Remove(vol);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VolExists(int id)
+        {
+            return _context.Vols.Any(e => e.Id == id);
+        }
+    }
+}
